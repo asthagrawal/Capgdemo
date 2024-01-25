ce60bcaae791d74778b651bf3fa418c853f5f878
Astha Agrawal <astha.agrawal@capgemini.com>
2024-01-25 09:45:00 +0530

    adding greet java file

---
+++ Greet.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class Greet {
+    
+    public String says()
+    {
+        return "Hello";
+    }
+
+    // static void Addition()
+    // {
+    //     // Scanner sc = new Scanner(System.in);
+    //     // int n1 = sc.nextInt();
+    //     // int n2 = sc.nextInt();
+
+    //     // if(n1<5 || n2<5)
+    //     // {
+    //     //     System.out.println("Invalid input");
+    //     // }
+    //     // else 
+    //     // {
+    //     //    int  sum = n1+n2;
+    //     //    System.out.println("Sum" +sum);
+    //     // //    return sum;
+    //     // }
+        
+    // }
+
+    public int add(int a, int b) {
+		return a+b;
+    }
+	
+}
